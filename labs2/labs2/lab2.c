dc25d4e79ae114d2fb54fa38fb20f9c977aad494
yurist03 <yura.kapichnikov.07@bk.ru>
2025-09-16 17:10:46 +0300

    Add files via upload

---
+++ labs2/labs2/lab2.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+
+	setlocale(LC_CTYPE, "");
+
+	int price = 20;
+	int width = 20;
+	int length = 80;
+
+	printf("Стоимость 1 кв метра: %d рублей\n", price);
+	printf("Ширина ткани: %d м., Длинна ткани: %d м.\n", width, length);
+	printf("Площадь ткани: %d кв. метров.\n", width * length);
+	printf("Стоимость всей ткани: %d рублей\n", width * length * price);
+
+}
